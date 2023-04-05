e28868c29bc5f8b75a6b873537dcf31fae3030c5
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-04-06 01:45:45 +0200

    Add some features

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,19 @@
+class StaticPagesController < ApplicationController
+  def welcome
+    features = [
+      "Choose from premade quizzes on a variety of topics",
+      "Make your own quizzes to customize your learning",
+      "Compare your scores with other users"
+    ]
+       
+    respond_to do |format|
+      format.html { render :welcome, locals: { features: features } }
+    end
+  end
+
+  def about
+    respond_to do |format|
+      format.html { render :about }
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: redirect('/welcome', status: 302)
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'welcome', to: 'static_pages#welcome', as: 'welcome'
+  get 'about', to: 'static_pages#about', as: 'about'
+
 end
